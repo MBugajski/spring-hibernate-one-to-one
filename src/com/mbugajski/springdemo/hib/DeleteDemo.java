00be5063f16e00d37de21c663720702d5ffe8c80
MBugajski <michalbugajski@outlook.com>
2021-02-10 15:31:19 +0100

    Set up and tested deleting objects with CascadeType.ALL

---
+++ src/com/mbugajski/springdemo/hib/DeleteDemo.java
@@ -0,0 +1,40 @@
+package com.mbugajski.springdemo.hib;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.mbugajski.springdemo.hib.entity.Instructor;
+import com.mbugajski.springdemo.hib.entity.InstructorDetail;
+import com.mbugajski.springdemo.hib.entity.Student;
+
+public class DeleteDemo {
+
+	public static void main(String[] args) {
+
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+
+		Session session = factory.getCurrentSession();
+
+		try {
+			
+			session.beginTransaction();
+			int theId = 1;
+			
+			Instructor tempInstructor = session.get(Instructor.class, theId); 
+			
+			if(tempInstructor != null) {
+				session.delete(tempInstructor);
+			}
+//			This will also delete the InstructorDetail object linked to tempInstructor due to CascadeType.ALL settings.
+			session.getTransaction().commit();
+		} finally {
+			factory.close();
+		}
+	}
+
+}
